8738d8cbd6a815717de2dddbbc088fb98d225fca
Jawad Khan <jawadkhan10322@gmail.com>
2023-07-26 16:36:55 +0500

    first commit

---
+++ 1.syntax.py
@@ -0,0 +1,10 @@
+#  Python is lossely typed language or dynamically typed language
+
+# in python indentation is very sensitive bcz python use indentations as blocks
+
+
+# in python print() is used for output 
+#  and input() is used for input
+
+print("hello World")
+x = input(" enter input here : ") 
---
+++ 3.NumericDT.py
@@ -0,0 +1,15 @@
+#  Numeric Data Types integer , float , complex
+
+# Integer --- integer have simple numeric value without decimal points
+
+
+age = 19
+print(age)
+ 
+
+#  float --- float have  decimal values
+pi = 3.14
+print(pi)
+# complex  as you know in maths there are complex numbers they have one real part and one imagnary 
+complexnumber = 3 + 9j
+print(complexnumber)
